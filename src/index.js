701974bde2836c6d53475a813838e40204dd94a5
“Young <hogusong1@gmail.com>
2019-07-30 11:30:41 -0400

    Create 'drawing cards' modules with pressing 'Start Game' button.

--- src/index.js
+++ src/index.js
@@ -14,11 +14,26 @@ let canDraw = [], canDrawId = null, timer, delayTime = 3000;
 init();
 
 function init() {
+  // Update player's data in LocalStorage after each game done.
+  if (gameStarted) ctrl.storeInStorage(players); 
 
   inAddOrRemove = false;          // to control buttons -> Start game, Add, Remove
   gameStarted = false;            // to control the status for in Game or not.
   settingPlayer(players);         // Show available players in the table
   dom.dScore.innerText = ' -- last score : ' + dealer.getScore();
+
+  dom.btnStart.onclick = () => {
+    if (base.canAcceptClick(gameStarted, inAddOrRemove)) {
+      createCanDraw();            // Check the players who joined the game.
+      if (canDraw.length < 1) {
+        message('Wait until players join the game!');
+      } else {
+        gameStarted = true;       // Set the status in-game mode.
+        initialDraw()             // Draw 2 cards for dealer and all in-play players
+        console.log(players);
+      }
+    }
+  }
 
   dom.btnAdd.onclick = () => {
     if (base.canAcceptClick(gameStarted, inAddOrRemove)) {
@@ -60,4 +75,31 @@ function init() {
       }
     })
   })
+}
+
+// Collect valid players in the 'canDraw' to control drawing card.
+function createCanDraw() {
+  canDraw = [];
+  players.forEach((p,i) => {
+    if (p.getInPlay()) canDraw.push(i);
+    else p.setPrevResult("didn't play");
+  });
+}
+
+function initialDraw() {
+  // Shuffe cards again when too little cards left as avaiable.
+  if (cards.length < 24) cards = base.shuffleCards();
+  dealer.emptyOnHand();         // Make empty the dealer's onHand[].
+  dom.dScore.innerText = '';
+
+  drawCard(dealer)
+  players.forEach(player => drawCard(player));
+  drawCard(dealer)
+  players.forEach(player => drawCard(player));
+}
+
+function drawCard(player) {
+  if (!player.getInPlay()) return;
+  const card = cards.pop();
+  player.addOnHand(card);      // Add the drawn card to the player's hand.
 }
